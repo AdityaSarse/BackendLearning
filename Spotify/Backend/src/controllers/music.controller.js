23ccee4b3a6cf05dad0c2bde188a4cfcc6622421
AdityaSarse <adityasarse362@gmail.com>
2026-06-22 01:10:39 +0530

    Album routes

--- Spotify/Backend/src/controllers/music.controller.js
+++ Spotify/Backend/src/controllers/music.controller.js
@@ -2,8 +2,8 @@ const { JsonWebTokenError } = require("jsonwebtoken")
 const musicModel = require("../models/music.model")
 const JWT = require("jsonwebtoken")
 const bcryptjs = require("bcryptjs");
-const { decode } = require("node:punycode");
 const storageService = require("../services/storage.service");
+const albumModel = require("../models/album.model");
 
 
 async function uploadMusic(req, res) {
@@ -76,6 +76,59 @@ async function uploadMusic(req, res) {
     }
 
 }
+
+async function createAlbum(req, res) {
+
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(403).json({
+            message: "Please login first"
+        })
+    }
+
+    let decoded;
+    try {
+        decoded = JWT.verify(token, process.env.JWT_SECRET);
+
+        if (decoded.role !== "artist") {
+            return res.status(403).json({
+                message: "You are not authorized to create album"
+            })
+        }
+    } catch (error) {
+        return res.status(403).json({
+            message: "Invalid token"
+        })
+    }
+
+    const { name } = req.body;
+    if (!name) {
+        return res.status(400).json({
+            message: "Album name is required"
+        })
+    }
+
+    let album;
+    try {
+        album = await albumModel.create({
+            name: req.body.name,
+            artist: decoded.id,
+        })
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Internal server error during album creation",
+            error: error.message
+        })
+    }
+
+    return res.status(201).json({
+        message: "Album created successfully",
+        album
+    })
+}
 module.exports = {
-    uploadMusic
+    uploadMusic,
+    createAlbum
 }
