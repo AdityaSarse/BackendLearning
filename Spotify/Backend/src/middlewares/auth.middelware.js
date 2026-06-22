ebd660bfd84524578fd5996ca08ab220c970b2c5
AdityaSarse <adityasarse362@gmail.com>
2026-06-22 12:32:39 +0530

    Adding middelwares for authentication

---
+++ Spotify/Backend/src/middlewares/auth.middelware.js
@@ -0,0 +1,35 @@
+const JWT = require("jsonwebtoken")
+
+async function authArtist(req, res, next) {
+
+    const token = req.cookies.token
+    if (!token) {
+        return res.status(403).json({
+            message: "Please login first"
+        })
+    }
+    try {
+        const decoded = JWT.verify(token, process.env.JWT_SECRET)
+
+        if (decoded.role !== "artist") {
+            return res.status(403).json({
+                message: "You are not authorized to perform this action"
+            })
+        }
+
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.log(error);
+        if (error.name === "JsonWebTokenError" || error.name === "TokenExpiredError") {
+            return res.status(403).json({
+                message: "Invalid token"
+            })
+        }
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+module.exports = authArtist;
