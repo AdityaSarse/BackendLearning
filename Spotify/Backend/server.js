2bfb85855badf3d2c6a6b4fa1b7e1ad81ce5b45d
AdityaSarse <adityasarse362@gmail.com>
2026-06-23 19:10:06 +0530

    remove routes

--- Spotify/Backend/server.js
+++ Spotify/Backend/server.js
@@ -4,8 +4,3 @@ const connectDB = require("./src/DB/db");
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 connectDB();
-
-
-app.get("/", (req, res) => {
-    res.send("Spotify Backend API is running 🚀");
-});
